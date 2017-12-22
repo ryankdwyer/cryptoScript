494e7509c8a212e949aa6cea83615aa99c7f2337
Ryan Dwyer <ryan@Ryans-MacBook-Pro-2.local>
2017-12-22 14:59:48 -0500

    volume and price change working

--- calculatorDaemon.js
+++ calculatorDaemon.js
@@ -11,41 +11,65 @@ const sortedSetKey = 'crypto_volume';
 (function () {
 
         
-    getLastEvents().then(function(events) {
-        let eventIds = events.reduce(function(prev, curr, i) {
-            if ( i % 2 === 1 ) {
-                prev.push(`event:${curr}`);
-            }
-            return prev;
-        }, []);
-        return getHashes(eventIds);
-    })
-    .then(function(hashes) {
-        let pairMap = {};
-        hashes.forEach((data) => {
-            let pair = `${data.FROMSYMBOL}${data.TOSYMBOL}`;
-            let dataObj = buildDataObj(data);
+    setInterval(function () {
+        getLastEvents().then(function(events) {
+            let eventIds = events.reduce(function(prev, curr, i) {
+                if ( i % 2 === 1 ) {
+                    prev.push(`event:${curr}`);
+                }
+                return prev;
+            }, []);
+            return getHashes(eventIds);
+        })
+        .then(function(hashes) {
+            let pairMap = {};
+            hashes.forEach((data) => {
+                let pair = `${data.FROMSYMBOL}${data.TOSYMBOL}`;
+                let dataObj = buildDataObj(data);
 
-            if ( pairMap[pair] ) {
-                pairMap[pair].push(dataObj);
-            } else {
-                pairMap[pair] = [dataObj]
+                if ( pairMap[pair] ) {
+                    pairMap[pair].push(dataObj);
+                } else {
+                    pairMap[pair] = [dataObj]
+                }
+            });
+
+            let volumeMap = {};
+            let priceMap = {};
+            for (var pair in pairMap) {
+                if (pairMap.hasOwnProperty(pair)) {
+                    volumeMap[pair] = calcChange(pairMap[pair], 'VOLUMEHOUR');
+                    priceMap[pair] = calcChange(pairMap[pair], 'PRICE');
+                }
             }
+            console.log(volumeMap, priceMap);
+            return hashes;
+        })
+        .then(function(hashes) {
+            hashes = hashes.map((el) => {
+                return `event:${el.id}`;
+            });
+            return removeHashes([hashes[0]]);
+        })
+        .then(function() {
+            return removeLastEvents();
+        })
+        .then(function(res) {
+            console.log(res);
         });
-
-        let volumeMap = {};
-        let priceMap = {};
-        for (var pair in pairMap) {
-            if (pairMap.hasOwnProperty(pair)) {
-                volumeMap[pair] = calcVolumeChange(pairMap[pair]);
-                priceMap[pair] = calcPriceChange(pairMap[pair]);
-            }
-        }
-        console.log(volumeMap);
-    })
+    }, 10000)
+    
 
     function getLastEvents () {
         return r.zrangeAsync(sortedSetKey, 0, 5, 'WITHSCORES');
+    }
+
+    function removeLastEvents () {
+        return r.zremrangebyrankAsync(sortedSetKey, 0, 1);
+    }
+
+    function removeHashes (keys) {
+        return r.delAsync(keys);
     }
 
     function getHashes (eventIds) {
@@ -68,14 +92,14 @@ const sortedSetKey = 'crypto_volume';
         return ret;
     }
 
-    function calcVolumeChange (data) {
+    function calcChange (data, key) {
         if (data.length < 5) return 0;
         deriv = 0;
         coeff = [1,-8,0,8,-1];
         h = 2;
         N = 5;
         for (var i = 0; i < N; i++) {
-            deriv += (data[i].VOLUMEHOUR * coeff[i]);
+            deriv += (data[i][key] * coeff[i]);
         }
         deriv /= ( 12 * h );
         return deriv;
